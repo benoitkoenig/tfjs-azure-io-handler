a44ed9e44d433496dcf46daeb8c6e7e4c2a434be
Benoit Koenig <koenigbenoit47@gmail.com>
2025-01-20 11:03:21 +0100

    Add a try/catch block to clear up resources in integration tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -32,51 +32,57 @@ describe("createAzureIoHandler", () => {
 
     await originalModel.save(savingHandler);
 
-    const savedAndLoadedModel = await tf.loadLayersModel(loadingHandler);
+    try {
+      const savedAndLoadedModel = await tf.loadLayersModel(loadingHandler);
 
-    const { $originalModelPrediction, $savedAndLoadedModelPrediction } =
-      tf.tidy(() => {
-        const $mockInput = tf.tensor([[1, 2, 3, 4, 5, 6]]);
+      const { $originalModelPrediction, $savedAndLoadedModelPrediction } =
+        tf.tidy(() => {
+          const $mockInput = tf.tensor([[1, 2, 3, 4, 5, 6]]);
 
-        return {
-          $originalModelPrediction: originalModel.predict(
-            $mockInput,
-          ) as tf.Tensor2D,
-          $savedAndLoadedModelPrediction: savedAndLoadedModel.predict(
-            $mockInput,
-          ) as tf.Tensor2D,
-        };
-      });
+          return {
+            $originalModelPrediction: originalModel.predict(
+              $mockInput,
+            ) as tf.Tensor2D,
+            $savedAndLoadedModelPrediction: savedAndLoadedModel.predict(
+              $mockInput,
+            ) as tf.Tensor2D,
+          };
+        });
 
-    const [originalModelPrediction, savedAndLoadedModelPrediction] =
+      const [originalModelPrediction, savedAndLoadedModelPrediction] =
+        await Promise.all([
+          $originalModelPrediction.array(),
+          $savedAndLoadedModelPrediction.array(),
+        ]);
+
+      $originalModelPrediction.dispose();
+      $savedAndLoadedModelPrediction.dispose();
+
+      // Explicitely check that they are both equal to `[[22]]` instead of checking that they are equal to each other
+      // to ensure that the test does not pass on a flawed result, eg. if the test returns an empty array
+      expect(originalModelPrediction).toStrictEqual([[22]]);
+      expect(savedAndLoadedModelPrediction).toStrictEqual([[22]]);
+
+      originalModel.dispose();
+      savedAndLoadedModel.dispose();
+    } finally {
+      /**
+       * Failing to dispose the models in case an error is acceptable within integration tests,
+       * so this try/finally block focuses exclusively on clearing up resources on azure which matters more.
+       */
+      const containerClient = getContainerClient(
+        savingHandlerParams ?? loadingHandlerParams,
+      );
+
       await Promise.all([
-        $originalModelPrediction.array(),
-        $savedAndLoadedModelPrediction.array(),
+        containerClient.deleteBlob(`${path}/model.json`, {
+          deleteSnapshots: "include",
+        }),
+        containerClient.deleteBlob(`${path}/weights.bin`, {
+          deleteSnapshots: "include",
+        }),
       ]);
-
-    $originalModelPrediction.dispose();
-    $savedAndLoadedModelPrediction.dispose();
-
-    // Explicitely check that they are both equal to `[[22]]` instead of checking that they are equal to each other
-    // to ensure that the test does not pass on a flawed result, eg. if the test returns an empty array
-    expect(originalModelPrediction).toStrictEqual([[22]]);
-    expect(savedAndLoadedModelPrediction).toStrictEqual([[22]]);
-
-    originalModel.dispose();
-    savedAndLoadedModel.dispose();
-
-    const containerClient = getContainerClient(
-      savingHandlerParams ?? loadingHandlerParams,
-    );
-
-    await Promise.all([
-      containerClient.deleteBlob(`${path}/model.json`, {
-        deleteSnapshots: "include",
-      }),
-      containerClient.deleteBlob(`${path}/weights.bin`, {
-        deleteSnapshots: "include",
-      }),
-    ]);
+    }
   }
 
   it("should save and load a model from azure using the storageAccountKey", async () => {
@@ -126,7 +132,7 @@ describe("createAzureIoHandler", () => {
   it("should throw when trying to save a model to azure using anonymous authentication", async () => {
     await expect(
       testIoHandler(
-        `createIoHandler-integration-test-anonymous-${new Date().toISOString()}`,
+        `createIoHandler-integration-test-saving-anonymous-${new Date().toISOString()}`,
         {
           containerName: "tfjs-azure-io-handler-with-anonymous-access",
           storageAccount: process.env["AZURE_STORAGE_ACCOUNT"]!,
