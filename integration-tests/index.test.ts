1444aa51e04b4f5e51f5c6746decb6389a627923
Benoit Koenig <koenigbenoit47@gmail.com>
2025-01-21 11:58:01 +0100

    Move unit tests in integration-tests/

--- integration-tests/index.test.ts
+++ integration-tests/index.test.ts
@@ -1,12 +1,9 @@
 import { describe, expect, it } from "vitest";
-import * as tf from "@tensorflow/tfjs-node";
 import {
   AnonymousCredential,
   StorageSharedKeyCredential,
 } from "@azure/storage-blob";
-import type { ContainerClientParams } from "./get-container-client";
-import createAzureIoHandler from ".";
-import getContainerClient from "./get-container-client";
+import testIoHandler from "./test-io-handler";
 
 // Environment variable names are picked to be consistent with https://learn.microsoft.com/en-us/azure/storage/blobs/authorize-data-operations-cli#set-environment-variables-for-authorization-parameters
 const AZURE_STORAGE_ACCOUNT = process.env["AZURE_STORAGE_ACCOUNT"];
@@ -18,82 +15,6 @@ if (!AZURE_STORAGE_ACCOUNT || !AZURE_STORAGE_KEY || !AZURE_STORAGE_SAS_TOKEN) {
 }
 
 describe("createAzureIoHandler", () => {
-  async function testIoHandler(
-    path: string,
-    loadingHandlerParams: ContainerClientParams,
-    savingHandlerParams?: ContainerClientParams,
-  ) {
-    const loadingHandler = createAzureIoHandler(path, loadingHandlerParams);
-    const savingHandler = savingHandlerParams
-      ? createAzureIoHandler(path, savingHandlerParams)
-      : loadingHandler;
-
-    const originalModel = tf.sequential({
-      layers: [
-        tf.layers.inputLayer({ inputShape: [6] }),
-        tf.layers.dense({
-          units: 1,
-          kernelInitializer: "ones",
-          biasInitializer: "ones",
-        }),
-      ],
-    });
-
-    await originalModel.save(savingHandler);
-
-    try {
-      const savedAndLoadedModel = await tf.loadLayersModel(loadingHandler);
-
-      const { $originalModelPrediction, $savedAndLoadedModelPrediction } =
-        tf.tidy(() => {
-          const $mockInput = tf.tensor([[1, 2, 3, 4, 5, 6]]);
-
-          return {
-            $originalModelPrediction: originalModel.predict(
-              $mockInput,
-            ) as tf.Tensor2D,
-            $savedAndLoadedModelPrediction: savedAndLoadedModel.predict(
-              $mockInput,
-            ) as tf.Tensor2D,
-          };
-        });
-
-      const [originalModelPrediction, savedAndLoadedModelPrediction] =
-        await Promise.all([
-          $originalModelPrediction.array(),
-          $savedAndLoadedModelPrediction.array(),
-        ]);
-
-      $originalModelPrediction.dispose();
-      $savedAndLoadedModelPrediction.dispose();
-
-      // Explicitely check that they are both equal to `[[22]]` instead of checking that they are equal to each other
-      // to ensure that the test does not pass on a flawed result, eg. if the test returns an empty array
-      expect(originalModelPrediction).toStrictEqual([[22]]);
-      expect(savedAndLoadedModelPrediction).toStrictEqual([[22]]);
-
-      originalModel.dispose();
-      savedAndLoadedModel.dispose();
-    } finally {
-      /**
-       * Failing to dispose the models in case an error is acceptable within integration tests,
-       * so this try/finally block focuses exclusively on clearing up resources on azure which matters more.
-       */
-      const containerClient = getContainerClient(
-        savingHandlerParams ?? loadingHandlerParams,
-      );
-
-      await Promise.all([
-        containerClient.deleteBlob(`${path}/model.json`, {
-          deleteSnapshots: "include",
-        }),
-        containerClient.deleteBlob(`${path}/weights.bin`, {
-          deleteSnapshots: "include",
-        }),
-      ]);
-    }
-  }
-
   it("should save and load a model from azure using the storageAccountKey", async () => {
     await testIoHandler(
       `createIoHandler-integration-test-storageAccountKey-${new Date().toISOString()}`,
---
+++ integration-tests/test-io-handler.ts
@@ -0,0 +1,82 @@
+import * as tf from "@tensorflow/tfjs-node";
+import { expect } from "vitest";
+
+import createAzureIoHandler from "src";
+import type { ContainerClientParams } from "src/get-container-client";
+import getContainerClient from "src/get-container-client";
+
+export default async function testIoHandler(
+  path: string,
+  loadingHandlerParams: ContainerClientParams,
+  savingHandlerParams?: ContainerClientParams,
+) {
+  const loadingHandler = createAzureIoHandler(path, loadingHandlerParams);
+  const savingHandler = savingHandlerParams
+    ? createAzureIoHandler(path, savingHandlerParams)
+    : loadingHandler;
+
+  const originalModel = tf.sequential({
+    layers: [
+      tf.layers.inputLayer({ inputShape: [6] }),
+      tf.layers.dense({
+        units: 1,
+        kernelInitializer: "ones",
+        biasInitializer: "ones",
+      }),
+    ],
+  });
+
+  await originalModel.save(savingHandler);
+
+  try {
+    const savedAndLoadedModel = await tf.loadLayersModel(loadingHandler);
+
+    const { $originalModelPrediction, $savedAndLoadedModelPrediction } =
+      tf.tidy(() => {
+        const $mockInput = tf.tensor([[1, 2, 3, 4, 5, 6]]);
+
+        return {
+          $originalModelPrediction: originalModel.predict(
+            $mockInput,
+          ) as tf.Tensor2D,
+          $savedAndLoadedModelPrediction: savedAndLoadedModel.predict(
+            $mockInput,
+          ) as tf.Tensor2D,
+        };
+      });
+
+    const [originalModelPrediction, savedAndLoadedModelPrediction] =
+      await Promise.all([
+        $originalModelPrediction.array(),
+        $savedAndLoadedModelPrediction.array(),
+      ]);
+
+    $originalModelPrediction.dispose();
+    $savedAndLoadedModelPrediction.dispose();
+
+    // Explicitely check that they are both equal to `[[22]]` instead of checking that they are equal to each other
+    // to ensure that the test does not pass on a flawed result, eg. if the test returns an empty array
+    expect(originalModelPrediction).toStrictEqual([[22]]);
+    expect(savedAndLoadedModelPrediction).toStrictEqual([[22]]);
+
+    originalModel.dispose();
+    savedAndLoadedModel.dispose();
+  } finally {
+    /**
+     * Failing to dispose the models in case an error is acceptable within integration tests,
+     * so this try/finally block focuses exclusively on clearing up resources on azure which matters more.
+     */
+    const containerClient = getContainerClient(
+      savingHandlerParams ?? loadingHandlerParams,
+    );
+
+    await Promise.all([
+      containerClient.deleteBlob(`${path}/model.json`, {
+        deleteSnapshots: "include",
+      }),
+      containerClient.deleteBlob(`${path}/weights.bin`, {
+        deleteSnapshots: "include",
+      }),
+    ]);
+  }
+}
